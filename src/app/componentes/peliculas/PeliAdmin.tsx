c571568361f2da98189c33eb248bc1b946865a68
Willim_Palacio <wpalacio3@cuc.edu.co>
2024-05-09 21:51:35 -0500

    peliAdmin

---
+++ src/app/componentes/peliculas/PeliAdmin.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Pelicula } from "../../modelos/Pelicula";
+import { FIX_PELICULAS } from "../../mocks/Pelicula-mocks";
+import { FIX_PELICULA_GENERO } from "../../utilidades/dominios/DomGenero";
+
+export const PeliAdmin = () => {
+  const [arrPeliculas] = useState<Pelicula[]>(FIX_PELICULAS);
+
+  const obtenerNombreGenero = (valor: string) => {
+    for (const objGen of FIX_PELICULA_GENERO) {
+      if (objGen.codGenero === valor) {
+        return objGen.nombreGenero;
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <div className="col-md-11 mt-4">
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr className="table-danger">
+                <th style={{ width: "10%" }}>Código</th>
+                <th style={{ width: "30%" }}>Nombre</th>
+                <th style={{ width: "20%" }}>Género</th>
+                <th style={{ width: "30%" }}>Protagonista</th>
+                <th style={{ width: "10%" }}>Imagen</th>
+                <th style={{ width: "10%" }}>Acciones</th> 
+              </tr>
+            </thead>
+            <tbody>
+              {arrPeliculas.map((miPeli: Pelicula) => (
+                <tr className="align-middle" key={miPeli.codPelicula}>
+                  <td>{miPeli.codPelicula}</td>
+                  <td>{miPeli.nombrePelicula}</td>
+                  <td>{obtenerNombreGenero(miPeli.codGeneroPelicula)}</td>
+                  <td>{miPeli.protagonistaPelicula}</td>
+                  <td>
+                    <img src={miPeli.imagenPeliculaBase64} alt="" className="imagenListado" />
+                    <div className="text-info">{miPeli.imagenPelicula}</div>
+                  </td>
+                  <td className="text-center">
+                    <a href="/eliminar/{miPeli.codPelicula}">
+                      <i className="fa-solid fa-trash-can rojito"></i>
+                    </a>
+                    {" "}
+                    <a href="/editar/{miPeli.codPelicula}">
+                      <i className="fa-regular fa-pen-to-square verde"></i>
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
